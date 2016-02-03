12a0c6666c291e0977dc90db7cdeb4ec30f23e75
Ji Hyun <nswwangjin@google.com>
2016-02-03 13:28:58 +0900

    첫 번째 커밋

---
+++ app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hji.myfirstandroidapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        // 화면에 layout 표시.
+    }
+}
