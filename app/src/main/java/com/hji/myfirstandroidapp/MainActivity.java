942b1ef1aef8a7d2fc484184133304680c215264
Ji Hyun <nswwangjin@google.com>
2016-02-04 14:35:36 +0900

    button click event 1.xml에 onClick 속성정의

--- app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
@@ -1,15 +1,24 @@
 package com.hji.myfirstandroidapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.button);
         // 화면에 layout 표시.
+    }
+
+    //activity_main.xml 에 연결됨.
+    public void onClick(View view) {
+        Log.d(TAG, "클릭 잘 됨");
     }
 }
