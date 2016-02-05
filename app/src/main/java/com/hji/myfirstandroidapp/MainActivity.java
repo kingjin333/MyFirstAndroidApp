c8f243eeee6303e5a5127251233e78034da97f91
Ji Hyun <nswwangjin@google.com>
2016-02-05 09:43:56 +0900

    button click event 4 - 여러버튼을 한메소드에서 처리. - 쓸데없는 로컬 변수 제거.

--- app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.hji.myfirstandroidapp;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
@@ -14,15 +13,23 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.button);
+        setContentView(R.layout.exam_list);
         // 화면에 layout 표시.
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(this);
+//        Button button2 = (Button) findViewById(R.id.button2);
+//        button2.setOnClickListener(this);
+
+        findViewById(R.id.button3).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(MainActivity.this, "클릭 매우 잘됨", Toast.LENGTH_SHORT).show();
+        switch (v.getId()) {
+            case R.id.button2:
+                Toast.makeText(MainActivity.this, "button2", Toast.LENGTH_SHORT).show();
+            case R.id.button3:
+                Toast.makeText(MainActivity.this, "button3", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
 
