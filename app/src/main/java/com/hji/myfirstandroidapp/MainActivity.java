82da18ec98e386bb56be78c3ea2eb4f62df16be7
Ji Hyun <nswwangjin@google.com>
2016-02-05 10:21:18 +0900

    check_box

--- app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
@@ -2,40 +2,35 @@ package com.hji.myfirstandroidapp;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements CompoundButton.OnCheckedChangeListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_box);
+        // 화면에 layout 표시.표시하고픈 xml명 변경.
 
-        setContentView(R.layout.exam_list);
-        // 화면에 layout 표시.
-//        Button button2 = (Button) findViewById(R.id.button2);
-//        button2.setOnClickListener(this);
-
-        findViewById(R.id.button3).setOnClickListener(this);
+        mTextView = (TextView) findViewById(R.id.msg_text_view);
+        ((CheckBox) findViewById(R.id.check_box)).setOnCheckedChangeListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button2:
-                Toast.makeText(MainActivity.this, "button2", Toast.LENGTH_SHORT).show();
-            case R.id.button3:
-                Toast.makeText(MainActivity.this, "button3", Toast.LENGTH_SHORT).show();
-
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Toast.makeText(MainActivity.this, "check" + isChecked, Toast.LENGTH_SHORT).show();
+        //text뷰에 글자를 변경.
+        if (isChecked) {
+            mTextView.setText("체크됨");
+        } else {
+            mTextView.setText("");
         }
     }
 }
 
-    //activity_main.xml 에 연결됨.
-//    public void onClick(View view) {
-//        Log.d(TAG, "클릭 잘 됨");
-//        Toast.makeText(MainActivity.this, "클릭잘됨", Toast.LENGTH_SHORT).show();
-//    }
-//}
