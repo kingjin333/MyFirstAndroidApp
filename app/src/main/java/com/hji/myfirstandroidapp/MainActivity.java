3fdc4671425d3c623da86ce538cfc95171e3a2d5
Ji Hyun <nswwangjin@google.com>
2016-02-15 11:16:56 +0900

    - Intent
    리턴.

--- app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/MainActivity.java
@@ -1,47 +1,64 @@
+
 package com.hji.myfirstandroidapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
-    private TextView mTextView;
+    public static final int REQUEST_CODE_PICTURE = 1000;
+    public static final int REQUEST_CODE_CAMERA = 2000;
     private EditText mNameEditText;
     private EditText mAgeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // 화면에 layout 표시.표시하고픈 xml명 변경.
-        findViewById(R.id.next_activity_button).setOnClickListener(this);
 
+        // 화면에 layout 표시
+        setContentView(R.layout.activity_main);
+
+        // 이름, 나이
         mNameEditText = (EditText) findViewById(R.id.name_edit_text);
         mAgeEditText = (EditText) findViewById(R.id.age_edit_text);
-        // 이름 나이
-        findViewById(R.id.next_activity_button).setOnClickListener(this);
-        //이벤트
-    }
 
+        // 이벤트
+        findViewById(R.id.next_activity_button).setOnClickListener(this);
+
+    }
 
     @Override
     public void onClick(View v) {
-
-        Toast.makeText(MainActivity.this, mNameEditText.getText().toString(), Toast.LENGTH_SHORT);
-        // 이름 나이 가져오기
-        Intent intent = new Intent(this, SecondActivity.class);
         // SecondActivity 로 전환하겠다는 intent
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        // 이름, 나이 가져와서 intent에 추가
         intent.putExtra("name", mNameEditText.getText().toString());
         intent.putExtra("age", mAgeEditText.getText().toString());
-        // 이름 나이 가져와서 intent에 추가
-        startActivity(intent);
+
         // intent의 정보를 토대로 다른 Activity를 시작
+        startActivityForResult(intent, REQUEST_CODE_PICTURE);
+    }
+
+    // 결과를 처리
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_PICTURE
+                || resultCode == RESULT_OK
+                || data != null) {
+            // 사진을 선택
+            String result = data.getStringExtra("result");
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+
+        } else if (requestCode == REQUEST_CODE_CAMERA) {
+            // 사진찍어서 받을 때 처리
+        }
     }
 }
-
--- app/src/main/java/com/hji/myfirstandroidapp/SecondActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/SecondActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * Created by 현 on 2016-02-05.
@@ -39,7 +38,12 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
         // 현재 Activity 종료, back key 누른 것과 동일.
-        Toast.makeText(SecondActivity.this, "종료합니다", Toast.LENGTH_SHORT).show();
-        finish();
+//        Toast.makeText(SecondActivity.this, "종료합니다", Toast.LENGTH_SHORT).show();
+//        finish();
+        Intent intent = new Intent();
+        intent.putExtra("result", mNameTextView.getText().toString() + ", " +
+                mAgeTextView.getText().toString());
+
+        setResult(MainActivity.RESULT_OK, intent);
     }
 }
