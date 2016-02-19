7bf27157b24c20579fa3d2fa3bf016d314115363
Ji Hyun <nswwangjin@google.com>
2016-02-19 14:14:32 +0900

    LongClick시 item 삭제.
    Grid View

--- app/src/main/java/com/hji/myfirstandroidapp/exam_ListView/ListViewActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/exam_ListView/ListViewActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,6 +17,10 @@ import java.util.List;
 public class ListViewActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListView mListView;
+    private GridView mGridView;
+    private ArrayAdapter<String> mAdapter;
+    private List <String> mData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +29,19 @@ public class ListViewActivity extends AppCompatActivity implements AdapterView.O
 
         // View
         mListView = (ListView) findViewById(R.id.list);
+        mGridView = (GridView) findViewById(R.id.grid);
         // Data
-        List<String>data = new ArrayList<>();
+        mData = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            data.add("data" + i);
+            mData.add("data" + i);
         }
 
         // Adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,data);
+        mAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,mData);
 
-        mListView.setAdapter(adapter);
+        mListView.setAdapter(mAdapter);
+        mGridView.setAdapter(mAdapter);
 
         // 클릭 이벤트
 
@@ -53,6 +60,14 @@ public class ListViewActivity extends AppCompatActivity implements AdapterView.O
     @Override
     public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(ListViewActivity.this, "long click : " + position, Toast.LENGTH_SHORT).show();
+
+        // 데이터 삭제
+        mData.remove(position);
+
+        // 화면 갱신 : Adapter 에게 데이터 변경을 알려준다.
+        // -> ListView 에 새로운 내용을 반영
+        mAdapter.notifyDataSetChanged();
+
         return true;
     }
 }
