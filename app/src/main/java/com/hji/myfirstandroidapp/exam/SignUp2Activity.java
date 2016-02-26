aa50c63c53dfc9cf9d11e8de99c94303febabde6
Ji Hyun <nswwangjin@google.com>
2016-02-26 15:38:41 +0900

    로그인 예제. 라디오버튼 사용

--- app/src/main/java/com/hji/myfirstandroidapp/exam/SignUp2Activity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/exam/SignUp2Activity.java
@@ -14,6 +14,7 @@ public class SignUp2Activity extends AppCompatActivity {
     private TextView mIdTextView;
     private TextView mPasswordTextView;
     private TextView mEmailTextView;
+    private TextView mRadioTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class SignUp2Activity extends AppCompatActivity {
         mIdTextView = (TextView) findViewById(R.id.id_text);
         mPasswordTextView = (TextView) findViewById(R.id.password_text);
         mEmailTextView = (TextView) findViewById(R.id.email_text);
+        mRadioTextView = (TextView) findViewById(R.id.male_text);
 
         // 나를 호출한 Intent 를 얻는다
         Intent intent = getIntent();
@@ -32,11 +34,13 @@ public class SignUp2Activity extends AppCompatActivity {
             String id = intent.getStringExtra("id");
             String passWord = intent.getStringExtra("password");
             String email = intent.getStringExtra("email");
-
+            String radio = intent.getStringExtra("radio");
 
             mIdTextView.setText(mIdTextView.getText().toString() + id);
             mPasswordTextView.setText(mPasswordTextView.getText().toString() + passWord);
             mEmailTextView.setText(mEmailTextView.getText().toString() + email);
+            mRadioTextView.setText(mRadioTextView.getText().toString() + radio);
+
         }
 
 
--- app/src/main/java/com/hji/myfirstandroidapp/exam/SignUpActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/exam/SignUpActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.hji.myfirstandroidapp.R;
@@ -17,6 +19,9 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private EditText mPassWordEditText;
     private EditText mEmailEditText;
     private EditText mPassWord2EditText;
+    private RadioGroup mRadioGroup;
+    private RadioButton mRadioman;
+    private RadioButton mRadiowoman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,46 +37,64 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         button1.setOnClickListener(this);
         Button button2 = (Button) findViewById(R.id.sign_up_btn);
         button2.setOnClickListener(this);
-        
+        mRadioGroup = (RadioGroup) findViewById(R.id.radio_group);
+        mRadioGroup.setOnClickListener(this);
+
+        mRadioman = (RadioButton) findViewById(R.id.man_radio);
+        mRadiowoman = (RadioButton) findViewById(R.id.woman_radio);
+
 
         mIdEditText = (EditText) findViewById(R.id.id_edit_text);
         mPassWordEditText = (EditText) findViewById(R.id.password_edit_text);
         mPassWord2EditText = (EditText) findViewById(R.id.password_edit_text2);
         mEmailEditText = (EditText) findViewById(R.id.email_edit_text);
 
-
     }
 
     @Override
     public void onClick(View v) {
+        // SignUp2 로 전환하겠다는 intent
+        Intent intent = new Intent(this, SignUp2Activity.class);
+
+        // id, password 를 가져와서 intent 에 추가
+        intent.putExtra("id", mIdEditText.getText().toString());
+        intent.putExtra("password", mPassWordEditText.getText().toString());
+        intent.putExtra("email", mEmailEditText.getText().toString());
+
+
+        if (mRadioGroup.getCheckedRadioButtonId() == R.id.man_radio) {
+            intent.putExtra("radio", mRadioman.getText().toString());
+        }
+        if (mRadioGroup.getCheckedRadioButtonId() == R.id.woman_radio) {
+            intent.putExtra("radio", mRadiowoman.getText().toString());
+
+        }
+
         switch (v.getId()) {
 
             case R.id.cho_gi_btn:
-                mIdEditText = null;
-                mPassWordEditText = null;
-                mPassWord2EditText = null;
-                mEmailEditText = null;
+                mIdEditText.setText(null);
+                mPassWordEditText.setText(null);
+                mPassWord2EditText.setText(null);
+                mEmailEditText.setText(null);
+                mRadioGroup.clearCheck();
 
                 break;
             case R.id.sign_up_btn:
 
-                    if (mPassWordEditText.getText().toString().equals(mPassWord2EditText.getText().toString())) {
-                        // SignUp2 로 전환하겠다는 intent
-                        Intent intent = new Intent(this, SignUp2Activity.class);
+                if (!(mPassWordEditText.getText().toString().equals(mPassWord2EditText.getText().toString()))) {
 
-                        // id, password 를 가져와서 intent 에 추가
-                        intent.putExtra("id", mIdEditText.getText().toString());
-                        intent.putExtra("password", mPassWordEditText.getText().toString());
-                        intent.putExtra("email", mEmailEditText.getText().toString());
+                    Toast.makeText(SignUpActivity.this, "비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show();
+                }
 
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(SignUpActivity.this, "비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show();
-                    }
-                if (mIdEditText.length() == 0 | mPassWordEditText.length() == 0 | mEmailEditText.length() == 0) {
+                if (mIdEditText.length() == 0 | mPassWordEditText.length() == 0 | mEmailEditText.length() == 0
+                        | mRadioGroup.getCheckedRadioButtonId() == 0) {
                     Toast.makeText(SignUpActivity.this, "모두 입력해 주셔야 합니다.", Toast.LENGTH_SHORT).show();
 
+                } else {
+                    startActivity(intent);
                 }
+
                 break;
 
 
