87a8396dd35eadb1a06ef54aa3c63cb454e49563
Ji Hyun <nswwangjin@google.com>
2016-03-08 11:04:08 +0900

    메모장 EditFragment

--- app/src/main/java/com/hji/myfirstandroidapp/notepad/NoteMainActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/notepad/NoteMainActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.hji.myfirstandroidapp.R;
+import com.hji.myfirstandroidapp.notepad.fragments.MemoEditFragment;
 
 public class NoteMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,11 +24,11 @@ public class NoteMainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_main);
 
-            // 액션바 대체.
+        // 액션바 대체.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-            // FloatingActionButton
+        // FloatingActionButton
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,18 +37,24 @@ public class NoteMainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-            // NavigationDrawer
+        // NavigationDrawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-            // NavigationDrawer 메뉴 리스너 연결 부분
+        // NavigationDrawer 메뉴 리스너 연결 부분
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // 기본 화면
+        getSupportFragmentManager().beginTransaction().replace(R.id.contents1,new MemoEditFragment()
+        ).commit();
     }
-        // 백키를 눌럿을때 네비게이션 드로어가 열려잇으면 닫고, 아니면 종료.
+
+    // 백키를 눌럿을때 네비게이션 드로어가 열려잇으면 닫고, 아니면 종료.
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -57,14 +64,16 @@ public class NoteMainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-        // 오른쪽 상단 메뉴 생성.
+
+    // 오른쪽 상단 메뉴 생성.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.note_main, menu);
         return true;
     }
-        // 오른쪽 상단 메뉴 선택시 처리.
+
+    // 오른쪽 상단 메뉴 선택시 처리.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -79,27 +88,20 @@ public class NoteMainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
-         //왼쪽 메뉴 처리.
+
+    //왼쪽 메뉴 처리.
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_memos) {
+        //TODO 메모 프래그먼트 표시
+        } else if (id == R.id.nav_settings) {
+        //TODO 설정 프래그먼트 표시
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
---
+++ app/src/main/java/com/hji/myfirstandroidapp/notepad/fragments/MemoEditFragment.java
@@ -0,0 +1,27 @@
+package com.hji.myfirstandroidapp.notepad.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hji.myfirstandroidapp.R;
+
+/**
+ * Created by 현 on 2016-03-08.
+ */
+public class MemoEditFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_memo_edit, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("메모");
+    }
+}
