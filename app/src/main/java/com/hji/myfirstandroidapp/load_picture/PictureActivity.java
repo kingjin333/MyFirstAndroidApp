23e89718e6b85218650506392d4c12b8c390942c
Ji Hyun <nswwangjin@google.com>
2016-03-23 15:29:35 +0900

    메모장 - 내보내기

--- app/src/main/java/com/hji/myfirstandroidapp/load_picture/PictureActivity.java
+++ app/src/main/java/com/hji/myfirstandroidapp/load_picture/PictureActivity.java
@@ -3,6 +3,7 @@ package com.hji.myfirstandroidapp.load_picture;
 import android.app.LoaderManager;
 import android.content.Context;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -16,7 +17,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.hji.myfirstandroidapp.MainActivity;
 import com.hji.myfirstandroidapp.R;
 import com.suwonsmartapp.abl.AsyncBitmapLoader;
 
@@ -96,6 +99,48 @@ public class PictureActivity extends AppCompatActivity implements LoaderManager.
 
         static class ViewHolder {
             ImageView imageView;
+        }
+    }
+
+    /**
+     * Created by 현 on 2016-02-05.
+     */
+    public static class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+        private TextView mAgeTextView;
+        private TextView mNameTextView;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_second);
+            mNameTextView = (TextView) findViewById(R.id.name_text_view);
+            mAgeTextView = (TextView) findViewById(R.id.age_text_view);
+
+            findViewById(R.id.exit_button).setOnClickListener(this);
+
+
+            //나를 호출한 Intent 를 얻는다
+            Intent intent = getIntent();
+            if (intent != null) {
+                // 이름 나이를 세팅
+                String name = intent.getStringExtra("name");
+                String age = intent.getStringExtra("age");
+
+                mNameTextView.setText(mNameTextView.getText().toString() + name);
+                mAgeTextView.setText(mAgeTextView.getText().toString() + age);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            // 현재 Activity 종료, back key 누른 것과 동일.
+    //        Toast.makeText(SecondActivity.this, "종료합니다", Toast.LENGTH_SHORT).show();
+    //        finish();
+            Intent intent = new Intent();
+            intent.putExtra("result", mNameTextView.getText().toString() + ", " +
+                    mAgeTextView.getText().toString());
+
+            setResult(MainActivity.RESULT_OK, intent);
         }
     }
 }
