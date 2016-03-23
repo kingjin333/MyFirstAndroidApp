64615b6dd4a0b54be63b16aabf4efb05ee7a8c2f
Ji Hyun <nswwangjin@google.com>
2016-03-23 16:20:27 +0900

    RecyclerAdapter 완성.

--- app/src/main/java/com/hji/myfirstandroidapp/notepad/adapters/MemoCursorAdapter.java
+++ app/src/main/java/com/hji/myfirstandroidapp/notepad/adapters/MemoCursorAdapter.java
@@ -15,16 +15,14 @@ import com.hji.myfirstandroidapp.notepad.db.MemoContract;
  * Created by 현 on 2016-03-10.
  */
 public class MemoCursorAdapter extends CursorAdapter {
-    private final LayoutInflater mInflator;
 
     public MemoCursorAdapter(Context context, Cursor c) {
         super(context, c, false);
-        mInflator = LayoutInflater.from(context);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View convertView = mInflator.inflate(R.layout.item_memo, parent, false);
+        View convertView = LayoutInflater.from(context).inflate(R.layout.item_memo, parent, false);
         ViewHolder holder = new ViewHolder();
         holder.title = (TextView) convertView.findViewById(R.id.title_text);
         holder.memo = (TextView) convertView.findViewById(R.id.memo_text);
--- app/src/main/java/com/hji/myfirstandroidapp/notepad/adapters/MemoRecyclerAdapter.java
+++ app/src/main/java/com/hji/myfirstandroidapp/notepad/adapters/MemoRecyclerAdapter.java
@@ -1,33 +1,68 @@
 package com.hji.myfirstandroidapp.notepad.adapters;
 
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hji.myfirstandroidapp.R;
+import com.hji.myfirstandroidapp.notepad.db.MemoContract;
 
 /**
  * Created by 현 on 2016-03-23.
  */
 public class MemoRecyclerAdapter extends RecyclerView.Adapter<MemoRecyclerAdapter.Holder> {
+    private final Cursor mCursor;
+
+    public MemoRecyclerAdapter(Cursor cursor) {
+        mCursor = cursor;
+    }
+
 
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_memo, parent, false);
+        return new Holder(itemView);
     }
 
     @Override
     public void onBindViewHolder(Holder holder, int position) {
+        Cursor cursor = mCursor;
+        cursor.moveToPosition(position);
+
+        String title = cursor.getString(cursor.getColumnIndexOrThrow(MemoContract.MemoEntry.COLUMN_NAME_TITLE));
+        String memo = cursor.getString(cursor.getColumnIndexOrThrow(MemoContract.MemoEntry.COLUMN_NAME_MEMO));
+        String date = cursor.getString(cursor.getColumnIndexOrThrow(MemoContract.MemoEntry.COLUMN_NAME_DATE));
+
+        holder.title.setText(title);
+        holder.memo.setText(memo);
+        holder.date.setText(date);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (mCursor == null) {
+            return 0;
+        }
+        return mCursor.getCount();
+//        return mCursor == null ? 0 : mCursor.getCount();
     }
 
+
     public static class Holder extends RecyclerView.ViewHolder {
+        TextView title;
+        TextView memo;
+        TextView date;
 
         public Holder(View itemView) {
             super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.title_text);
+            memo = (TextView) itemView.findViewById(R.id.memo_text);
+            date = (TextView) itemView.findViewById(R.id.date_text);
         }
     }
 }
